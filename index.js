b64f03607ee82e1d3d7216c976b09e7f8d1915f0
Stanlo06-arch <Stanlo06@web.de>
2026-05-02 23:28:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ const {
 const TOKEN = process.env.TOKEN;
 
 // ===== CONFIG =====
-const PANEL_CHANNEL_ID = "PANEL_ID";
-const LOGO = "DEIN_LOGO_LINK";
-const BANNER = "DEIN_BANNER_LINK";
+const PANEL_CHANNEL_ID = "1498441200062169159";
+const LOGO = "https://cdn.discordapp.com/attachments/1475610426766262333/1495199546035146822/Top_Gear.png";
+const BANNER = "https://cdn.discordapp.com/attachments/1475610426766262333/1496968229585944676/ChatGPT_Image_23._Apr._2026_20_49_03.png";
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
